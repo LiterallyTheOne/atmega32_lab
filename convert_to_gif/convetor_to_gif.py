ba3eb27419b66846d54b97d9629fd8b98ca38ec7
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2025-02-16 22:01:21 +0330

    Rename to convertor_to_gif.py

--- convert_to_gif/convetor_to_gif.py
+++ convert_to_gif/convetor_to_gif.py
@@ -4,14 +4,14 @@ from moviepy.editor import VideoFileClip
 
 
 def convert_to_gif(input_path, output_path):
-    """Convert mp4 video to a GIF file."""
+    """Convert a video to a GIF file."""
     clip = VideoFileClip(input_path)
     clip.write_gif(output_path)
 
 
 def main():
     """main function"""
-    input_path = "mp_file.mp4"
+    input_path = "haha.webm"
     output_path = "output_video.gif"
     convert_to_gif(input_path, output_path)
 
