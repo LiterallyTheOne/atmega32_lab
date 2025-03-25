7ac399163a8a952e5ede031de99a0694ec07a38e
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2025-03-25 10:39:28 +0330

    Make pwm_init() better

--- src/s9_pwm_servo_adc.cpp
+++ src/s9_pwm_servo_adc.cpp
@@ -38,20 +38,25 @@ uint16_t adc_read(uint8_t channel)
 
 void pwm_init()
 {
-    // Set PD5 (OC1A) as output
+    // Set PD5 as output
     DDRD |= (1 << PD5);
 
+    // Make sure that there is no setup other than the one we are going to put
     TCCR1A = 0;
     TCCR1B = 0;
 
+    // non-inverting mode
     TCCR1A |= (1 << COM1A1);
 
+    // Fast PWM mode
     TCCR1A |= (1 << WGM11);
     TCCR1B |= (1 << WGM12) | (1 << WGM13);
 
+    // Set prescaler to 8
     TCCR1B |= (1 << CS11);
 
-    ICR1 = 20'000;
+    // Set the frequency to 50Hz
+    ICR1 = 19'999;
 }
 
 void setup()
