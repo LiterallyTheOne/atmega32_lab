8429dae677c94b746df76b3a5fdc4a7be9cf5750
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2025-03-25 12:45:36 +0330

    Update s9_pwm_servo_adc.cpp to make it more understandable based on my explanations

--- src/s9_pwm_servo_adc.cpp
+++ src/s9_pwm_servo_adc.cpp
@@ -1,38 +1,33 @@
 #include <Arduino.h>
 
-#include <util/delay.h>
-#include <lcd_library.h>
-
-uint32_t x = 1000;
-
-const int potPin = 0; // Potentiometer connected to Analog pin A0
-
-unsigned long z = 0;
-
-char toshow[16];
+uint16_t adc0_value;
 
 void adc_init()
 {
-    // Set the reference voltage to AVcc
-    ADMUX = (1 << REFS0);
+    // Set reference voltage to AVCC
+    ADMUX |= (1 << REFS0);
 
-    // Enable the ADC and set the prescaler to 128
-    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+    // Enable ADC
+    ADCSRA |= (1 << ADEN);
+
+    // Set prescaler to 128
+    ADCSRA |= (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2);
 }
 
 uint16_t adc_read(uint8_t channel)
 {
-    // Select the ADC channel with safety mask
-    ADMUX = (ADMUX & 0xF0) | (channel & 0x0F);
+    // Set the channel
+    ADMUX &= 0xF0;
+    ADMUX |= channel;
 
     // Start the conversion
     ADCSRA |= (1 << ADSC);
 
-    // Wait for the conversion to complete
+    // Wait for the conversion to finish
     while (ADCSRA & (1 << ADSC))
         ;
 
-    // Return the ADC value
+    // Retrun the value
     return ADC;
 }
 
@@ -61,27 +56,16 @@ void pwm_init()
 
 void setup()
 {
-    adc_init();
     pwm_init();
-    lcd_init();
-
-    // OCR1A = 1000;
+    adc_init();
 }
 
 void loop()
 {
-    uint16_t potValue = adc_read(potPin); // Read potentiometer (0-1023)
+    adc0_value = adc_read(0);
 
-    z = ((unsigned long)potValue * 1000) / 1023 + 1000;
+    adc0_value = map(adc0_value, 0, 1023, 1'000, 2'000);
 
-    OCR1A = z;
-
-    sprintf(toshow, "%lu", z);
-
-    lcd_gotoxy(0, 0);
-    lcd_clear();
-    _delay_ms(100);
-    lcd_puts(toshow);
-
-    _delay_ms(1000);
+    OCR1A = adc0_value;
+    delay(100);
 }
