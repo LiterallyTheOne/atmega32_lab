9f0dcbf962d422aaad778169e8f43e8972a22060
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2025-03-25 09:15:30 +0330

    Update the code to make it cooler

--- src/s9_pwm_servo_adc.cpp
+++ src/s9_pwm_servo_adc.cpp
@@ -41,9 +41,15 @@ void pwm_init()
     // Set PD5 (OC1A) as output
     DDRD |= (1 << PD5);
 
-    // Configure Timer1 for Fast PWM, Mode 14 (ICR1 as TOP)
-    TCCR1A = (1 << COM1A1) | (1 << WGM11);
-    TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Prescaler = 8
+    TCCR1A = 0;
+    TCCR1B = 0;
+
+    TCCR1A |= (1 << COM1A1);
+
+    TCCR1A |= (1 << WGM11);
+    TCCR1B |= (1 << WGM12) | (1 << WGM13);
+
+    TCCR1B |= (1 << CS11);
 
     ICR1 = 20'000;
 }
