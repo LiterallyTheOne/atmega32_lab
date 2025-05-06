69b7c1225074aa447b391ac27478f5c9008eeed0
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2025-05-06 10:04:47 +0330

    Add two_adc code and two adc simulation

---
+++ src/s9_two_adc.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#include <lcd_library.h>
+
+uint8_t adc0_value = 0;
+uint8_t adc1_value = 0;
+
+char adc0_str[4];
+char adc1_str[4];
+
+void init_adc()
+{
+    ADMUX |= (1 << REFS0);
+
+    ADCSRA |= (1 << ADEN);
+    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+}
+
+uint8_t read_adc(uint8_t channel)
+{
+    ADMUX &= 0xE0;
+    ADMUX |= channel;
+
+    ADCSRA |= (1 << ADSC);
+
+    while (ADCSRA & (1 << ADSC))
+        ;
+
+    return ADC;
+}
+
+void setup()
+{
+    init_adc();
+
+    lcd_init();
+}
+
+void loop()
+{
+    adc0_value = read_adc(0);
+    adc1_value = read_adc(1);
+
+    sprintf(adc0_str, "%d", adc0_value);
+    sprintf(adc1_str, "%d", adc1_value);
+
+    lcd_clear();
+    lcd_gotoxy(0, 0);
+    lcd_puts(adc0_str);
+    lcd_gotoxy(1, 0);
+    lcd_puts(adc1_str);
+
+    delay(500);
+}
